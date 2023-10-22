646d79853d190cc5742fb20e651324fdaf789b9b
TheRealMkadmi <wahib.mkadmi@esprit.tn>
2023-10-22 19:20:06 +0100

    🔒 feat(AppServiceProvider.php): force HTTPS scheme if app URL is not localhost for enhanced security The indentation in AppServiceProvider.php has been adjusted to improve code readability. 🔧 refactor(AppServiceProvider.php): adjust indentation for better readability Additionally, a new feature has been added to force the use of HTTPS if the application URL is not localhost. This change enhances the security of the application by ensuring that all data transferred between the web server and browser remains encrypted when the application is in a production environment.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,28 +3,32 @@
 namespace App\Providers;
 
 use App\Models\User;
+use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Cashier\Cashier;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
+  /**
+   * Register any application services.
+   *
+   * @return void
+   */
+  public function register()
+  {
+    //
+  }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        Cashier::useCustomerModel(User::class);
+  /**
+   * Bootstrap any application services.
+   *
+   * @return void
+   */
+  public function boot(UrlGenerator $url)
+  {
+    Cashier::useCustomerModel(User::class);
+    if (!str_contains(config('app.url'), 'localhost')) {
+      $url->forceScheme('https');
     }
+  }
 }
